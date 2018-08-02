bdd3c461006fc306d73432313e72203e910ff568
JonRooge <jonmrug@gmail.com>
2018-08-01 22:19:03 -0400

    reordered function. Issues resovled with incorrect bytes being printed. Confirmed it has correct bytes in buffer with HxD byte code viewer. Program loads into buffer via malloc now.

--- chip8.c
+++ chip8.c
@@ -1,34 +1,40 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 
+#define RAMSIZE 4096
 
-int loadFile(char * arg, char * membuffer[]){
-	FILE * fp = fopen(arg, "r");
-	
-	if (fscanf(fp, "%x", membuffer)){
-		return 1;
-	}
-	return 0;
-}
 
 
 int main(int argc, char ** argv){
-	int result = 0;
-	char * membuffer[38];
+	int result = 0,
+	    bytep = 0,
+	    fsize = 0;
+	uint8_t * membuffer;
+	FILE * fp = fopen(argv[1], "r");
 
-	if(argc == 1 || loadFile(argv[1], membuffer)){
+	fseek(fp, 0, SEEK_END);
+	fsize = ftell(fp);
+	membuffer = (uint8_t*) malloc(sizeof(uint8_t) * (fsize+1));
+
+	fseek(fp, 0, SEEK_SET);
+	if (fread(membuffer, 1, fsize, fp) < fsize) {
+		return 1;
+	}
+	
+	fclose(fp);
+	
+	if(argc == 1 || fsize <= 1){
 		printf("Failed to load file.");
 		return 1;
 	}
 
-	int fend = 38;
-	int pointer = 0;
-
-	while(pointer != fend){
-		printf("byte: %x\n", membuffer[pointer]);
-		pointer++;
+	while(bytep != fsize){
+		printf("%x ", membuffer[bytep]);
+		bytep++;
 	}
 	//result = decompile(f);
 	
 	return 0;
+
 }
